dc0dceac91fcaf8c6971b2749818376e5723ebbf
lilacbrowne <league@WTS22.attlocal.net>
2017-12-31 15:55:27 -0800

    array lists

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,12 +1,66 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame;
+	JPanel panel;
+	JButton bAdd;
+	JButton bView;
+	static ArrayList<String> guests;
+
+	public static void main(String[] args) {
+		guests = new ArrayList<String>();
+		GuestBook book = new GuestBook();
+		book.GUI();
+	}
+
+	public void GUI() {
+		frame = new JFrame("Guest Book");
+		panel = new JPanel();
+		bAdd = new JButton("Add Name");
+		bView = new JButton("View Names");
+		bAdd.addActionListener(this);
+		bView.addActionListener(this);
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(bAdd);
+		panel.add(bView);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource().equals(bAdd)) {
+			String add = JOptionPane.showInputDialog("Add Guest");
+			guests.add(add);
+		}
+		if (e.getSource().equals(bView)) {
+			String output = "";
+			for (String string : guests) {
+				output += string + "\n";
+			}
+			JOptionPane.showMessageDialog(null, output);
+		}
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,39 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
+		ArrayList<String> pets = new ArrayList();
+		// 2. Add five Strings to your list
+		pets.add("dog");
+		pets.add("cat");
+		pets.add("fish");
+		pets.add("bird");
+		pets.add("hamster");
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < pets.size(); i++) {
+			System.out.println(pets.get(i));
+		}
+		// 4. Print all the Strings using a for-each loop
+		for (String string : pets) {
+			System.out.println(string);
+		}
+		// 5. Print only the even numbered elements in the list.
+		for (int i = 0; i < pets.size(); i += 2) {
+			System.out.println(pets.get(i));
+		}
+		// 6. Print all the Strings in reverse order.
+		for (int i = pets.size() - 1; i >= 0; i--) {
+			System.out.println(pets.get(i));
+		}
+		// 7. Print only the Strings that have the letter 'e' in them.
+		for (String string : pets) {
+			if (string.contains("e")) {
+				System.out.println(string);
+			}
+		}
 	}
 }
